7aa145fdd776cf4fe3e540b643e7b455acbb7859
Santiago Gómez <gomezalonsosantiago@gmail.com>
2023-05-30 19:21:59 -0300

    Agregar gato y Exotico

--- project/config/apps/home/views.py
+++ project/config/apps/home/views.py
@@ -9,3 +9,14 @@ def agregar_perro(request):
     context={"form": form}
     return render(request, "home/agregar_perro.html", context)
 
+
+def agregar_gato(request):
+    form = forms.GatoForm(request.POST)
+    context={"form": form}
+    return render(request, "home/agregar_gato.html", context)
+
+
+def agregar_exotico(request):
+    form = forms.ExoticoForm(request.POST)
+    context={"form": form}
+    return render(request, "home/agregar_Exotico.html", context)
