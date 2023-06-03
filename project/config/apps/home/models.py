9dfd15cbfc8ca16cbe74d20972c709a3a707bde8
Santiago Gómez <gomezalonsosantiago@gmail.com>
2023-06-03 10:41:10 -0300

    cambios visuales a la web

--- project/config/apps/home/models.py
+++ project/config/apps/home/models.py
@@ -3,24 +3,26 @@ from django.db import models
 # Create your models here.
 
 class Perro(models.Model):
-    raza = models.CharField(max_length=100)
-    edad = models.IntegerField()
+    nombre = models.CharField(max_length=100)
+    años = models.IntegerField()
     peso = models.DecimalField(max_digits=5, decimal_places=2)
-
+    raza = models.CharField(max_length=100)
     def __str__(self):
         return self.raza  
 
 class Gato(models.Model):
-    raza = models.CharField(max_length=100)
-    edad = models.IntegerField()
+    nombre = models.CharField(max_length=100)
+    años = models.IntegerField()
     peso = models.DecimalField(max_digits=5, decimal_places=2)
+    raza = models.CharField(max_length=100)
 
     def __str__(self):
         return self.raza  
     
 class Exotico(models.Model):
-    raza = models.CharField(max_length=100)
-    edad = models.IntegerField()
+    nombre = models.CharField(max_length=100)
+    especie = models.CharField(max_length=100)
+    años = models.IntegerField()
     peso = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
